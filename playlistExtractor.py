93b064deea5e29c69c0d92a6f048c762588e06d7
ericj79 <ericj79@gmail.com>
2017-01-02 20:30:36 -0500

    Updated to work with Volumio 2

--- playlistExtractor.py
+++ playlistExtractor.py
@@ -1,4 +1,6 @@
 import ConfigParser
+import urllib
+import os;
 from pyItunes import *
 
 def parseConfig(filename):	# Start the function
@@ -20,19 +22,33 @@ def parseXml(settings):
   for name in playlists:
     if (name in settings['whiteList']):
       playlist = data.getPlaylist(name);
-      handle = open(settings['outputDir'].encode('utf-8') + '/'.encode('utf-8') + name.encode('utf-8') + '.m3u'.encode('utf-8'), 'w');
-      handle.write('#EXTM3U\n'.encode('utf-8'));
+      handle = open(settings['outputDir'].encode('utf-8') + '/'.encode('utf-8') + name.encode('utf-8'), 'w');
+      handle.write('['.encode('utf-8'));
+      first = True
       for song in playlist.tracks:
+        if first:
+          first = False
+        else:
+          handle.write(','.encode('utf-8'))
         path = song.location.replace(settings['pattern'], settings['replacement']).encode('utf-8');
-        handle.write('#EXTINF:'.encode('utf-8'))
+        handle.write('\n  {\n    "service": "mpd",\n    "type": "song",\n    "title" : "'.encode('utf-8'))
         if (song.name):
           handle.write(song.name.encode('utf-8'));
+        handle.write('",\n    "artist": "'.encode('utf-8'))
         if (song.artist):
-          handle.write(' - '.encode('utf-8') + song.artist.encode('utf-8'));
-        handle.write('\n'.encode('utf-8'));
-        handle.write(path + '\n'.encode('utf-8'));
+          handle.write(song.artist.encode('utf-8'));
+        handle.write('",\n    "album": "'.encode('utf-8'));
+        if (song.album):
+          handle.write(song.album.encode('utf-8'));
+        handle.write('",\n    "albumart": "'.encode('utf-8'));
+        albumPath = os.path.dirname(path);
+        shortPath = albumPath.replace(settings['replacement'], '');
+        handle.write('/albumart?web=' + urllib.quote(shortPath) + '/large&path=' + urllib.quote(albumPath) + '&icon=fa-dot-circle-o');
+        handle.write('",\n    "uri": "'.encode('utf-8'));
+        handle.write(path + '"\n  }'.encode('utf-8'));
         if (path == song.location):
             print 'Did not match: ' + song.location;
+      handle.write('\n]\n'.encode('utf-8'));
       handle.close();
 
 def main():
